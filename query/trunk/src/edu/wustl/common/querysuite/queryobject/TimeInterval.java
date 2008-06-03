7949df5585247d81f8c7bc1fe2bedd7c2490f92f
srinath_k <srinath_k@mail.nih.gov>
2008-06-03 08:40:27 +0000

    *** empty log message ***

--- query/trunk/src/edu/wustl/common/querysuite/queryobject/TimeInterval.java
+++ query/trunk/src/edu/wustl/common/querysuite/queryobject/TimeInterval.java
@@ -2,8 +2,4 @@ package edu.wustl.common.querysuite.queryobject;
 
 public enum TimeInterval {
     Minute, Hour, Day, Week, Month, Quarter, Year;
-    public static void main(String[] args) {
-        System.out.println(Minute.compareTo(Day));
-        System.out.println(Week.compareTo(Week));
-    }
 }
