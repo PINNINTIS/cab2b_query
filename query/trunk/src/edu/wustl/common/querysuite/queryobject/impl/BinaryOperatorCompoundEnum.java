660e8216d17b8e288a3e516c1d1ad38be299967e
srinath_k <srinath_k@mail.nih.gov>
2008-06-25 13:29:54 +0000

    removed map

--- query/trunk/src/edu/wustl/common/querysuite/queryobject/impl/BinaryOperatorCompoundEnum.java
+++ query/trunk/src/edu/wustl/common/querysuite/queryobject/impl/BinaryOperatorCompoundEnum.java
@@ -2,45 +2,17 @@ package edu.wustl.common.querysuite.queryobject.impl;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import edu.wustl.common.querysuite.queryobject.ArithmeticOperator;
 import edu.wustl.common.querysuite.queryobject.IBinaryOperator;
 import edu.wustl.common.querysuite.queryobject.LogicalOperator;
+import edu.wustl.common.util.CompoundEnum;
 
-public class BinaryOperatorCompoundEnum<T extends Enum<?> & IBinaryOperator> implements Serializable {
+public class BinaryOperatorCompoundEnum<T extends Enum<?> & IBinaryOperator>
+        extends
+            CompoundEnum<BinaryOperatorCompoundEnum<T>, T> implements Serializable {
     private static final long serialVersionUID = 7794336702657368626L;
-
-    private static class MapKey<K extends Enum<?> & IBinaryOperator> {
-        private K k;
-
-        MapKey(K k) {
-            if (k == null) {
-                throw new NullPointerException();
-            }
-            this.k = k;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (!(obj instanceof MapKey)) {
-                return false;
-            }
-            return k == ((MapKey<?>) obj).k;
-        }
-
-        @Override
-        public int hashCode() {
-            return k.hashCode();
-        }
-    }
-
-    private static final Map<MapKey<?>, BinaryOperatorCompoundEnum<?>> map = new HashMap<MapKey<?>, BinaryOperatorCompoundEnum<?>>();
 
     private static final List<IBinaryOperator> values = new ArrayList<IBinaryOperator>();
 
@@ -48,9 +20,14 @@ public class BinaryOperatorCompoundEnum<T extends Enum<?> & IBinaryOperator> imp
 
     private static int nextOrdinal = 1;
 
+    // STATIC INIT
     static {
         addEnums(LogicalOperator.class);
         addEnums(ArithmeticOperator.class);
+    }
+
+    private static int nextOrdinal() {
+        return nextOrdinal++;
     }
 
     private static <T extends Enum<?> & IBinaryOperator> void addEnums(Class<T> klass) {
@@ -61,41 +38,18 @@ public class BinaryOperatorCompoundEnum<T extends Enum<?> & IBinaryOperator> imp
     }
 
     private static <T extends Enum<?> & IBinaryOperator> BinaryOperatorCompoundEnum<T> newCompoundEnum(T e) {
-        BinaryOperatorCompoundEnum<T> compoundEnum = new BinaryOperatorCompoundEnum<T>(e.name(), nextOrdinal++, e);
-        map.put(mapKey(e), compoundEnum);
+        BinaryOperatorCompoundEnum<T> compoundEnum = new BinaryOperatorCompoundEnum<T>(e, nextOrdinal());
         return compoundEnum;
     }
 
-    private static <K extends Enum<?> & IBinaryOperator> MapKey<K> mapKey(K k) {
-        return new MapKey<K>(k);
-    }
-
-    private final String name;
-
-    private final int ordinal;
-
-    private final T primitiveEnum;
-
-    private BinaryOperatorCompoundEnum(String name, int ordinal, T primitiveEnum) {
-        this.name = name;
-        this.ordinal = ordinal;
-        this.primitiveEnum = primitiveEnum;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public int ordinal() {
-        return ordinal;
-    }
-
-    public T primitiveEnum() {
-        return primitiveEnum;
-    }
+    // END STATIC INIT
 
     public static <T extends Enum<?> & IBinaryOperator> BinaryOperatorCompoundEnum<T> compoundEnum(T primitiveEnum) {
-        return (BinaryOperatorCompoundEnum<T>) map.get(mapKey(primitiveEnum));
+        if (primitiveEnum == null) {
+            return null;
+        }
+        int index = values.indexOf(primitiveEnum);
+        return (BinaryOperatorCompoundEnum<T>) compoundValues.get(index);
     }
 
     public static IBinaryOperator[] values() {
@@ -104,5 +58,9 @@ public class BinaryOperatorCompoundEnum<T extends Enum<?> & IBinaryOperator> imp
 
     public static BinaryOperatorCompoundEnum<?>[] compoundValues() {
         return compoundValues.toArray(new BinaryOperatorCompoundEnum<?>[0]);
+    }
+
+    private BinaryOperatorCompoundEnum(T primitiveEnum, int ordinal) {
+        super(primitiveEnum, ordinal);
     }
 }
