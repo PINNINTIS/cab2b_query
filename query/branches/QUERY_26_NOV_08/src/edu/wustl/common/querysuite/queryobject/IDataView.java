72c1c79838df2a02a0d0f5b9ef84d124b7bff8e9
vijay_pande <vijay_pande@mail.nih.gov>
2008-12-04 06:24:20 +0000

    interface for data view

---
+++ query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/IDataView.java
@@ -0,0 +1,17 @@
+package edu.wustl.common.querysuite.queryobject;
+
+import java.util.List;
+
+
+/**
+ * @author vijay_pande
+ * Interface to declare operations for DataView.
+ */
+public interface IDataView extends IResultView
+{
+	/**
+	 * Method to return list of output attributes
+	 * @return outputAttributeList List of objects of type IOutputAttribute
+	 */
+	public List<IOutputAttribute> getOutputAttributeList();
+}
