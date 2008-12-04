0b01b0f28881ca3b48620b3255477f19ad33aac8
vijay_pande <vijay_pande@mail.nih.gov>
2008-12-04 06:24:58 +0000

    interface for operation required in composite query

---
+++ query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/IOperation.java
@@ -0,0 +1,27 @@
+package edu.wustl.common.querysuite.queryobject;
+
+
+/**
+ * @author vijay_pande
+ * Interface to declare operations for composite query operation
+ */
+public interface IOperation
+{
+	/**
+	 * Method to get id of the operation
+	 * @return id identifier object of Long
+	 */
+	public Long getId();
+	
+	/**
+	 * Method to get first operand for operation
+	 * @return query object of type IQuery
+	 */
+	public IQuery getOperandOne();
+	
+	/**
+	 * Method to get second operand for operation
+	 * @return query object of type IQuery
+	 */
+	public IQuery getOperandTwo();
+}
