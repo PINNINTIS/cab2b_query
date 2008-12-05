6874abe6d396d0932d5ca15c02140f09735ab84b
vijay_pande <vijay_pande@mail.nih.gov>
2008-12-05 06:34:56 +0000

    Identifiable interface implemented

--- query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/impl/CompositeQuery.java
+++ query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/impl/CompositeQuery.java
@@ -2,13 +2,14 @@ package edu.wustl.common.querysuite.queryobject.impl;
 
 import edu.wustl.common.querysuite.queryobject.ICompositeQuery;
 import edu.wustl.common.querysuite.queryobject.IOperation;
+import edu.wustl.common.util.Identifiable;
 
 
 /**
  * @author vijay_pande
  * Class for composite query
  */
-public class CompositeQuery extends AbstractQuery implements ICompositeQuery
+public class CompositeQuery extends AbstractQuery implements ICompositeQuery, Identifiable
 {
 	/**
 	 * Default serial version id
@@ -36,5 +37,14 @@ public class CompositeQuery extends AbstractQuery implements ICompositeQuery
 	public void setOperation(IOperation operation)
 	{
 		this.operation = operation;
+	}
+	
+	/**
+	 * Method to get object id of the composite query required while saving CSM data
+	 * @param objectid  Object of type String
+	 */
+	public String getObjectId() 
+	{
+		return this.getClass().getName()+ "_" + this.getId();
 	}
 }
