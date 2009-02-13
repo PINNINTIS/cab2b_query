cb38f20993cabf365da5d31a1590c0c326933a15
sumit_gurav <sumit_gurav@mail.nih.gov>
2009-02-13 05:30:20 +0000

    File is added to support MsSqlServer database.

---
+++ query/branches/autoRoundCF/src/edu/wustl/common/querysuite/utils/MsSqlServerPrimitiveOperationProcessor.java
@@ -0,0 +1,36 @@
+package edu.wustl.common.querysuite.utils;
+
+import edu.wustl.common.querysuite.queryobject.ArithmeticOperator;
+import edu.wustl.common.querysuite.queryobject.IDateLiteral;
+
+public class MsSqlServerPrimitiveOperationProcessor extends SQLPrimitiveOperationProcessor {
+	 
+	private static final String dateFormat="110";
+	private static final String strToDateFunc="convert";
+	
+	@Override
+    String getDateDiffString(String leftStr, String rightStr) {
+    	return "convert(numeric(20),DATEDIFF(minute, " + rightStr + ", " + leftStr + "))*60";
+    }
+    
+    @Override
+    String modifyDateLiteral(IDateLiteral s) {
+        return strToDateFunc + "(smalldatetime,'" + standardDateFormat(s) + "', " + dateFormat + ")";
+    }
+
+    @Override
+    String dateToTimestamp(String s) {
+        return s;
+    }
+
+    @Override
+    String getTimeOffsetOpString(String timeStr, String offsetStr, ArithmeticOperator operator) {
+    	
+        if (operator == ArithmeticOperator.Minus) {
+            // TODO check
+            offsetStr = "-" + offsetStr;
+        }
+        Integer offSetNumber=new Integer(offsetStr);
+        return "DATEADD(second, " + offSetNumber + ", " + timeStr + ")";
+    }       
+}
