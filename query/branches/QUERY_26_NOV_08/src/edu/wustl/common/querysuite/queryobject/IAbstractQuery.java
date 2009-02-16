b6f649d710eb7bcdfe633e103317102141f6e2e9
vijay_pande <vijay_pande@mail.nih.gov>
2009-02-16 11:09:32 +0000

    IAbstractQuery interface updated for type

--- query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/IAbstractQuery.java
+++ query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/IAbstractQuery.java
@@ -5,7 +5,8 @@ package edu.wustl.common.querysuite.queryobject;
  * @author vijay_pande
  *
  */
-public interface IAbstractQuery extends IBaseQueryObject
+public interface IAbstractQuery extends IBaseQueryObject, INameable, IDescribable
 {
-	
+	public String getType();
+	public void setType(String type);	
 }
--- query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/IParameterizedQuery.java
+++ query/branches/QUERY_26_NOV_08/src/edu/wustl/common/querysuite/queryobject/IParameterizedQuery.java
@@ -9,7 +9,7 @@ import java.util.List;
  * @author chetan_patil
  * @created Aug 31, 2007, 1:46:07 PM
  */
-public interface IParameterizedQuery extends IQuery, INameable, IDescribable {
+public interface IParameterizedQuery extends IQuery {
     List<IOutputAttribute> getOutputAttributeList();
 
     void setOutputAttributeList(List<IOutputAttribute> outputAttributeList);
